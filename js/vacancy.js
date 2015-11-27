123e14f89e61aa3fa625d6fe7b25bfcc6eef346b
Илья <verst.promocity.ekb@yandex.ru>
2015-11-27 13:46:21 +0500

    Прописал адреса ссылок

---
+++ js/vacancy.js
@@ -0,0 +1,43 @@
+      var vacancy_card_title_1 = document.querySelector(".vacancy-card:nth-of-type(1) h1");
+      var vacancy_card_container_1 = document.querySelector(".vacancy-card:nth-of-type(1) .vacancy-card-container");
+
+      vacancy_card_title_1.addEventListener("click", function(event){
+      	event.preventDefault();
+      	vacancy_card_container_1.classList.toggle("modal-content-show");      
+      });
+
+
+      var vacancy_card_title_2 = document.querySelector(".vacancy-card:nth-of-type(2) h1");
+      var vacancy_card_container_2 = document.querySelector(".vacancy-card:nth-of-type(2) .vacancy-card-container");
+
+      vacancy_card_title_2.addEventListener("click", function(event){
+      	event.preventDefault();
+      	vacancy_card_container_2.classList.toggle("modal-content-show");      
+      });
+
+
+      var vacancy_card_title_3 = document.querySelector(".vacancy-card:nth-of-type(3) h1");
+      var vacancy_card_container_3 = document.querySelector(".vacancy-card:nth-of-type(3) .vacancy-card-container");
+
+      vacancy_card_title_3.addEventListener("click", function(event){
+      	event.preventDefault();
+      	vacancy_card_container_3.classList.toggle("modal-content-show");      
+      });
+
+
+      var vacancy_card_title_4 = document.querySelector(".vacancy-card:nth-of-type(4) h1");
+      var vacancy_card_container_4 = document.querySelector(".vacancy-card:nth-of-type(4) .vacancy-card-container");
+
+      vacancy_card_title_4.addEventListener("click", function(event){
+      	event.preventDefault();
+      	vacancy_card_container_4.classList.toggle("modal-content-show");      
+      });
+
+
+      var vacancy_card_title_5 = document.querySelector(".vacancy-card:nth-of-type(5) h1");
+      var vacancy_card_container_5 = document.querySelector(".vacancy-card:nth-of-type(5) .vacancy-card-container");
+
+      vacancy_card_title_5.addEventListener("click", function(event){
+      	event.preventDefault();
+      	vacancy_card_container_5.classList.toggle("modal-content-show");      
+      });
